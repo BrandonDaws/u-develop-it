610a706655554bfc1707c372a7a03fed64319141
BrandonDaws <velarde.brandon02@gmail.com>
2022-05-12 18:57:28 -0500

    connected to mysql db

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const mysql = require('mysql2');
 
 //middleware for express
 app.use(express.urlencoded({extended: false }));
 app.use(express.json());
 
+//connect to the mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'election'
 
-
-
-
-
-
-
-app.get('/', (req,res) => {
-    res.json({
-        message: 'hello world'
-    });
-});
+    },
+    console.log('connected to the election database')
+);
 
 app.use((req,res) => {
     res.status(404).end();
